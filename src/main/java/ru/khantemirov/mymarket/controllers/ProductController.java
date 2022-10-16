7378c7be80bc61d40416a745eab91d1093988ebb
Nail <kotmebius@gmail.com>
2022-10-16 19:44:06 +0500

    GlobalExceptions

--- src/main/java/ru/khantemirov/mymarket/controllers/ProductController.java
+++ src/main/java/ru/khantemirov/mymarket/controllers/ProductController.java
@@ -2,11 +2,16 @@ package ru.khantemirov.mymarket.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.khantemirov.mymarket.entities.Product;
+import ru.khantemirov.mymarket.exceptions.AppError;
+import ru.khantemirov.mymarket.exceptions.ResourceNotFoundException;
 import ru.khantemirov.mymarket.services.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -18,10 +23,22 @@ public class ProductController {
     public List<Product> findAllProducts(){
         return productService.findAll();
     }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> findProductById(@PathVariable long id){
+//        Optional<Product> product = productService.findById(id);
+//        if (!product.isPresent()){
+//            ResponseEntity<AppError> err = new ResponseEntity<AppError>(new AppError(HttpStatus.NOT_FOUND.value(),
+//                    "Продукт не найден, id:"+id), HttpStatus.NOT_FOUND);
+//            return err;
+//        }
+//        return new ResponseEntity<>(product.get(), HttpStatus.OK);
+//    }
 
     @GetMapping("/{id}")
     public Product findProductById(@PathVariable long id){
-        return productService.findById(id).get();
+        return productService.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Продукт не найден, id:"+id));
     }
 
     @DeleteMapping("/{id}")
